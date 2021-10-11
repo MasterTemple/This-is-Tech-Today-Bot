9cb673be7fe8b99719601c2e7fb903b3f5b20d07
MasterTemple <dgmastertemple@protonmail.com>
2021-10-10 21:46:44 -0700

    roles are removed if they are selected and they already have it

--- selectMenus/role.js
+++ selectMenus/role.js
@@ -1,6 +1,21 @@
 module.exports = async(interaction, config) => {
+    let currentRoles = interaction.member.roles.cache
+    let added = false
+    let removed = false
     interaction.values.forEach((eachRole) => {
-        interaction.member.roles.add(eachRole)
+        if(currentRoles.has(eachRole)){
+            interaction.member.roles.remove(eachRole)
+            added = true
+        }else{
+            interaction.member.roles.add(eachRole)
+            removed = true
+        }
     })
-    interaction.reply({content: "Roles Added!", ephemeral: true})
+    if(added && removed){
+        interaction.reply({content: "Roles Updated!", ephemeral: true})
+    }else if(added){
+        interaction.reply({content: "Roles Added!", ephemeral: true})
+    }else if(remove){
+        interaction.reply({content: "Roles Removed!", ephemeral: true})
+    }
 }
